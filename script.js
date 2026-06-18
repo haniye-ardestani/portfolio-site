aa88189c99d9601af940af51fcd4bef3a35bca62
haniye-ardestani <ardestanihaniyez@gmail.com>
2026-06-18 23:12:20 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-document.querySelectorAll("nav a").forEach(link=>{
-  link.addEventListener("click", e=>{
-    e.preventDefault();
-    document.querySelector(link.getAttribute("href"))
-    .scrollIntoView({behavior:"smooth"});
-  });
-});
+let index = 0;
+const slides = document.querySelectorAll(".hero-slider img");
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 3000);
